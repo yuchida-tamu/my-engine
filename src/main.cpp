40977c71a6e00f148e9028b5210ad1d3f498a26c
Yuta Uchida <yuchida4dev@gmail.com>
2024-11-24 16:50:28 +0900

    feat: basic triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
 #include<glad/gl.h>
 #include<GLFW/glfw3.h>
 
@@ -17,10 +20,34 @@ void setUpGlfw(){
 #endif
 }
 
-void clearWindow(GLFWwindow* window){
-    glClearColor(0.07f, 0.07f, 0.07f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
+char* readShaderFile (const char* filename){
+    std::filesystem::path filepath(filename);
+
+    if (!std::filesystem::exists(filepath)) {
+        std::cerr << "File does not exist: " << filepath << std::endl;
+        return NULL;
+    }
+
+    std::ifstream file(filepath);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filepath << std::endl;
+        return NULL;
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string content = buffer.str();
+
+    char* data = (char*)malloc(content.size() + 1);
+    if (data == NULL) {
+        std::cerr << "Failed to allocate memory for: " << filepath << std::endl;
+        return NULL;
+    }
+
+    std::copy(content.begin(), content.end(), data);
+    data[content.size()] = '\0';
+
+    return data;
 }
 
 int main(){
@@ -38,12 +65,83 @@ int main(){
     gladLoadGL(glfwGetProcAddress);
     glViewport(0, 0, 800, 800);
 
-    clearWindow(window);
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
 
+    unsigned int vertexShader, fragShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* vertexShaderSource = readShaderFile("../src/shaders/basic.vert");
+    if (vertexShaderSource == NULL) {
+        std::cerr << "Failed to read vertex shader file" << std::endl;
+        return -1;
+    }
+    const char* fragShaderSource = readShaderFile("../src/shaders/basic.frag");
+    if (fragShaderSource == NULL) {
+        std::cerr << "Failed to read fragment shader file" << std::endl;
+        return -1;
+    }
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glShaderSource(fragShader, 1, &fragShaderSource, NULL);
+    glCompileShader(fragShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success){
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragShader);
+
+
+    unsigned int VAO,VBO;
+    glGenVertexArrays(1, &VAO);  
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glClearColor(0.07f, 0.07f, 0.07f, 1.0f);
+   
     while (!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
     glfwTerminate();
